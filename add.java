994232dcab401ebccbbd5ce95bd0b40200f163ac
Namratha Krisram <73338328+NAMSKN@users.noreply.github.com>
2021-11-10 19:44:51 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class add
+ {
+   public static void main (String args[])
+   {
+     int a = 10, b = 10;
+     int res;
+     res = a + b;
+     System.out.println("Addition of "+a+" and "+b+" is "+res);
+   }
+ }	
